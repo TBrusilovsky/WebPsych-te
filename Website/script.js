0b332f62a942fc6feae6b041cc79149c0bdd7828
TBrusilovsky <55800776+TBrusilovsky@users.noreply.github.com>
2021-03-27 14:33:10 -0400

    starter/pseudo code for some stuff

--- Website/script.js
+++ Website/script.js
@@ -1,13 +1,34 @@
-<!DOCTYPE html>
-<html lang='en'>
-  <head>
-    <meta charset='UTF-8'/>
-    <title>Slicker Schedueling</title>
-    <link rel='stylesheet' href='styles.css'/>
-  </head>
 
-  <body>
+let times = [];
+let numTimes = 0;
+let timeAvailable = 0;
+let tasks = [];
+let numTasks = 0;
+let timeNeeded = 0;
+let yourScheduel = [];
 
 
-  </body>
-</html>
+function addTime(event) {  //takes the data from the form and records it in some way
+  times[numTimes] = {date: --GetDate--, startTime: --GetTime--, endTime: --GetTime2--, length: --endTime - startTime --};
+  timeAvailable+=times[numTimes].length;
+  numTimes+=1;
+}
+
+function getTasks(event) { //creates the next screen? might be worth just making more html and doing this as a hyperlink
+
+}
+
+function addTask { //take the data from the form and records it in some way
+  tasks[numTasks] = {name: --GetName--, estimatedTime: --GetTimeEstimate--};
+  timeNeeded+=tasks[numTasks].estimatedTime;
+  numTasks+=1;
+}
+
+function getScheduel(event) { //switches you to the scheduel screen, as before might be worth just hyperlinking it
+  generateScheduel();
+  //creates/passes in the newly made scheduel
+}
+
+function generateScheduel() {
+
+}
