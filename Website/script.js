c9a5b9a68321229ad766111792c3d4de9d342d89
TBrusilovsky <55800776+TBrusilovsky@users.noreply.github.com>
2021-03-27 18:56:40 -0400

    wrote out some cleaner html and first inputs for js

--- Website/script.js
+++ Website/script.js
@@ -9,7 +9,12 @@ let yourScheduel = [];
 
 
 function addTime(event) {  //takes the data from the form and records it in some way
-  times[numTimes] = {date: --GetDate--, startTime: --GetTime--, endTime: --GetTime2--, length: --endTime - startTime --};
+  times[numTimes] = {date:document.getElementById("date").value, 
+    startTime: document.getElementById("starttime").value, 
+    endTime: document.getElementById("endtime").value, 
+    length: calculateTime(document.getElementById("starttime").value,document.getElementById("endtime").value)
+  };
+
   timeAvailable+=times[numTimes].length;
   numTimes+=1;
 }
@@ -18,18 +23,34 @@ function getTasks(event) { //creates the next screen? might be worth just making
 
 }
 
-function addTask { //take the data from the form and records it in some way
-  tasks[numTasks] = {name: --GetName--,due: --getDue--, estimatedTime: --GetTimeEstimate--};
+function addTask() { //take the data from the form and records it in some way
+  //tasks[numTasks] = {name: --GetName--,due: --getDue--, estimatedTime: --GetTimeEstimate--};
   timeNeeded+=tasks[numTasks].estimatedTime;
   numTasks+=1;
 }
 
-function getScheduel(event) { //switches you to the scheduel screen, as before might be worth just hyperlinking it
+function getSchedule(event) { //switches you to the schedule screen, as before might be worth just hyperlinking it
   generateScheduel();
-  //creates/passes in the newly made scheduel
+  //creates/passes in the newly made schedule
 }
 
-function generateScheduel() {
-yourScheduel[i] = {date: --, workingon: --, timeStart: --, timeend: --, timespent: --}
-  example - {date:"03272000", workingon:"hack", timeStart:"6:00", timeend: "7:00", timespent:"1"}
+function calculateTime(start,end) {
+  let endDec = 0;
+  let startDec = 0;
+
+  if (start.charAt(6) == 'p')  startDec+=12;
+  if (end.charAt(6) == 'p')  endDec+=12;
+  console.log(startDec);
+  startDec+= 0+ start.substring(0,2);
+  endDec+= 0+end.substring(0,2);
+console.log(startDec);
+  startDec+= 0+start.substring(3,5)/60;
+  endDec+= 0+end.substring(3,5)/60;
+console.log(startDec);
+  return endDec-startDec;
+}
+
+function generateSchedule() {
+//yourScheduel[i] = {date: --, workingon: --, timeStart: --, timeend: --, timespent: --}
+ // example - {date:"03272000", workingon:"hack", timeStart:"6:00", timeend: "7:00", timespent:"1"}
 }
