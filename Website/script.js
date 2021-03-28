1f0d45c4772ff1f38ba7449a788e4cf147542bce
TBrusilovsky <55800776+TBrusilovsky@users.noreply.github.com>
2021-03-27 22:20:45 -0400

    finished everything except addTask and schedule display

--- Website/script.js
+++ Website/script.js
@@ -20,8 +20,48 @@ function addTime(event) {  //takes the data from the form and records it in some
 }
 
 function getTasks(event) { //creates the next screen? might be worth just making more html and doing this as a hyperlink
+  //change old stuff!
+  let instructions = document.getElementById("instructions");
+  instructions.innerHTML = "Now input what work you need to get done!"
+  let thingtochange = document.getElementById("title1");
+  thingtochange.innerHTML = "Due Date:";
+  thingtochange = document.getElementById("title2");
+  thingtochange.innerHTML = "Task Name:";
+  thingtochange = document.getElementById("title3");
+  thingtochange.innerHTML = "Estimated time needed for completion (in hours):";
 
-}
+  thingtochange = document.getElementById("endtime");
+  thingtochange.id = "estimatedtime";
+  thingtochange.placeholder = "2.5";
+
+  thingtochange = document.getElementById("addTime");
+  thingtochange.remove();
+  thingtochange =  document.getElementById("continue");
+  thingtochange.remove();
+
+  let thingtoadd = document.createElement("div");
+  thingtoadd.class = "actions";
+  thingtoadd.id = "addEvent";
+  let otherthingtoadd = document.createElement("input");
+  thingtoadd.appendChild(otherthingtoadd);
+  otherthingtoadd.type="submit";
+  otherthingtoadd.addEventListener("click",addTask);
+  document.body.appendChild(thingtoadd);
+
+  thingtoadd = document.createElement("div");
+  thingtoadd.class = "continue";
+  thingtoadd.id = "addEvent";
+  otherthingtoadd = document.createElement("input");
+  thingtoadd.appendChild(otherthingtoadd);
+  otherthingtoadd.type="button";  
+  otherthingtoadd.addEventListener("click",getSchedule);
+  otherthingtoadd.value = "Continue";
+  document.body.appendChild(thingtoadd);
+
+  //<div class="actions" id="addTime"><input type="submit" value="Add Time" onclick="addTime();"></div>
+
+   // <div class="continue" id="continue"><input type="button" value="Continue" onclick="getTasks();" /></div> 
+} 
 
 function addTask() { //take the data from the form and records it in some way
   //tasks[numTasks] = {name: --GetName--,due: --getDue--, estimatedTime: --GetTimeEstimate--};
